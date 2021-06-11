13e2d14b630f494314a1c2c11c3ea9ec68178c77
Chan Gu <>
2021-06-11 14:48:44 +0900

    imporoved init setting, removed unnecessary set

--- PangleQuickStartDemo/app/src/main/java/com/pangleglobal/panglequickstartdemo/PangleApplication.kt
+++ PangleQuickStartDemo/app/src/main/java/com/pangleglobal/panglequickstartdemo/PangleApplication.kt
@@ -35,19 +35,14 @@ class PangleApplication : Application() {
         return TTAdConfig.Builder()
             // Please use your own appId, this is for demo
             .appId("5081617")
-            .appName(packageName)
             // Turn it on during the testing phase, you can troubleshoot with the log, remove it after launching the app
             .debug(BuildConfig.DEBUG)
-            // The default setting is SurfaceView.
+            // The default setting is SurfaceView. We strongly recommend to set this to true.
             // If using TextureView to play the video, please set this and add "WAKE_LOCK" permission in manifest
             .useTextureView(true)
-            // Allow show Notification
-            .allowShowNotify(true)
-            // Whether to support multi-process, true indicates support
-            .supportMultiProcess(false)
             // Fields to indicate whether you are a child or an adult ，0:adult ，1:child
             .coppa(0)
-            //Fields to indicate whether you are protected by GDPR,  the value of GDPR : 0 close GDRP Privacy protection ，1: open GDRP Privacy protection
+            //Fields to indicate whether you are protected by GDPR,  the value of GDPR. 0: close GDRP Privacy protection ，1: open GDRP Privacy protection
             .setGDPR(0)
             .build()
     }
